3914e53166c561ed3afc2af637b0acc50a739602
Blake Smith <suicideking2525@gmail.com>
2019-10-16 17:53:28 -0500

    Add project files.

---
+++ GameFramework/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    class Game
+    {
+        //Wheither or not the Game should finish running and exit
+        public static bool GameOver = false;
+
+        private Scene _currentScene;
+
+        public Game()
+        {
+            _currentScene = new Scene();
+        }
+
+        public void Run()
+        {
+            Player player = new Player();
+            player.X = 4;
+            player.Y = 0;
+
+            Entity enemy = new Entity('#');
+            enemy.X = 7;
+            enemy.Y = 2;
+
+            _currentScene.AddEntity(player);
+            _currentScene.AddEntity(enemy);
+
+            _currentScene.Start();
+
+            //loops until the game is over
+            while(!GameOver)
+            {
+                _currentScene.Update();
+                _currentScene.Draw();
+                PlayerInput.ReadKey();
+            }
+        }
+
+        public Scene CurrentScene
+        {
+            get
+            {
+                return _currentScene;
+            }
+        }
+    }
+}
---
+++ GameFramework/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    class Player : Entity
+    {
+        public Player() : this('@')
+        {
+           
+        }
+
+        public Player(char icon) : base(icon)
+        {
+            //OnUpdate += MoveRight;
+            PlayerInput.AddKeyEvent(MoveRight, ConsoleKey.RightArrow);
+            PlayerInput.AddKeyEvent(MoveLeft, ConsoleKey.LeftArrow);
+            PlayerInput.AddKeyEvent(MoveUp, ConsoleKey.UpArrow);
+            PlayerInput.AddKeyEvent(MoveDown, ConsoleKey.DownArrow);
+        }
+
+        //Moves 1 space to the right
+        private void MoveRight()
+        {
+            if (X < TheScene.SizeX - 1)
+            {
+                X++;
+            }
+        }
+
+        //Moves 1 space to the left
+        private void MoveLeft()
+        {
+            if (X > 0)
+            {
+                X--;
+            }
+        }
+
+        private void MoveUp()
+        {
+            if (Y > 0)
+            {
+                Y--;
+            }
+        }
+
+        private void MoveDown()
+        {
+            if (Y < TheScene.SizeY -1)
+            {
+                Y++;
+            }
+        }
+    }
+}
